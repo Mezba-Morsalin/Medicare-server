d644c4b2c88eab001797678fd795f7ccdd3044ce
Mezba Morsalin <mejbahmorsalin560@hotmail.com>
2026-06-22 02:19:41 +0600

    Server Created

---
+++ index.js
@@ -0,0 +1,45 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const app = express();
+dotenv.config()
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT
+const uri = process.env.MONGO_URI
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+     const db = client.db(process.env.AUTH_DB_NAME)
+    // Send a ping to confirm a successful connection
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to  Medicare Server')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
